10dd960063ce5f58c154dfa8ad6a58ba1c76b5ba
Samuel Lujan <samuel_lujan@hotmail.com>
2020-07-09 10:44:04 -0300

    Produto e associacao Muito para muitos

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Produto;
 import com.example.cursomc.repositories.CategoriaRepository;
+import com.example.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,8 +30,20 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(c1);
+		p2.getCategorias().addAll(Arrays.asList(c1,c2));
+		p3.getCategorias().add(c1);
+		
 		
 		categoriaRepository.saveAll(Arrays.asList(c1,c2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.example.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -16,6 +19,11 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
+	
 	public Integer getId() {
 		return id;
 	}
@@ -27,6 +35,13 @@ public class Categoria implements Serializable{
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	public Categoria() {
@@ -62,7 +77,4 @@ public class Categoria implements Serializable{
 		return true;
 	}
 	
-	
-	
 }
-
